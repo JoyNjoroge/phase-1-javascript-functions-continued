5761255874a9aa02eaf717e9aa9d7803a15b5eb7
Joy Njoroge <njorogejoy78@gmail.com>
2025-03-18 22:06:05 +0300

    Updated index.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function saturdayFun (activity = 'roller-skate') {
+   return (`This Saturday, I want to ${activity}!`);
+}
+saturdayFun('bathe my dog');
+
+function mondayWork (activityUno = 'go to the office') {
+    return (`This Monday, I will ${activityUno}.`);
+}
+mondayWork('work from home')
+
+function wrapAdjective (flair = "*") {
+    return function (adjective = "special"){
+       return`You are ${flair}${adjective}${flair}!`;
+    }
+}
